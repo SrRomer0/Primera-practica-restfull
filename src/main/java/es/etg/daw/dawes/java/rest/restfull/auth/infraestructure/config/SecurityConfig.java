c05b1da4367fdabaea6ac55c3dab65e8becf4535
Diego <diego.romero.vera.11@gmail.com>
2026-01-27 16:19:19 +0000

    dev container

--- src/main/java/es/etg/daw/dawes/java/rest/restfull/auth/infraestructure/config/SecurityConfig.java
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/auth/infraestructure/config/SecurityConfig.java
@@ -1,19 +1,20 @@
 package es.etg.daw.dawes.java.rest.restfull.auth.infraestructure.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import es.etg.daw.dawes.java.rest.restfull.auth.domain.Permiso;
-import es.etg.daw.dawes.java.rest.restfull.auth.domain.Rol;
 import es.etg.daw.dawes.java.rest.restfull.auth.infraestructure.db.jpa.repository.UserEntityRepository;
 import es.etg.daw.dawes.java.rest.restfull.auth.infraestructure.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
@@ -21,9 +22,8 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
-    
-    private final UserEntityRepository repository;
 
+    private final UserEntityRepository repository;
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -35,7 +35,6 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
@@ -51,17 +50,50 @@ public class SecurityConfig {
     //     http.httpBasic(withDefaults());
     //     return http.build();
     // }
-    
+
+    // @Bean
+    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    //     http.authorizeHttpRequests((requests) -> requests
+    //         .requestMatchers("/categorias/**").hasRole(Rol.ADMIN.name())
+    //         .requestMatchers("/productos/**").hasAnyRole(Rol.USER.name(), Rol.ADMIN.name())
+    //         .requestMatchers(HttpMethod.GET, "/productos/**").hasAuthority(Permiso.ADMIN_READ.getPermiso())
+    //         .anyRequest().authenticated());
+    //     http.formLogin(withDefaults());
+    //     http.httpBasic(withDefaults());
+    //     return http.build();
+    // }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+        .cors(cors -> cors.configure(http)) // Habilita CORS dentro de Spring Security
+        .csrf(csrf -> csrf.disable()) // Deshabilitar CSRF para pruebas (opcional)
+        .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
     
-        http.authorizeHttpRequests((requests) -> requests
-            .requestMatchers("/categorias/**").hasRole(Rol.ADMIN.name())
-            .requestMatchers("/productos/**").hasAnyRole(Rol.USER.name(), Rol.ADMIN.name())
-            .requestMatchers(HttpMethod.GET, "/productos/**").hasAuthority(Permiso.ADMIN_READ.getPermiso())
-            .anyRequest().authenticated());
-        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
         return http.build();
     }
+
+    /** Configuración para evitar el error de CORS que dará el navegador */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        
+        // Permitimos todos los orígenes
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        
+        // Permitimos los métodos necesarios
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        
+        // Permitimos todas las cabeceras
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        
+        // Permitimos que el navegador reciba la respuesta
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    
 }
